ec673ac2cac95e1de6d02bcde8945817e7b4b6f4
Lurhakongola <lurhakongola@gmail.com>
2026-05-20 14:00:26 +0300

    prepare for render deployment

--- practicalServer/index.js
+++ practicalServer/index.js
@@ -4,9 +4,7 @@ const Note = require('./models/note')
 const morgan = require('morgan')
 const path = require('path')
 const mongoose = require('mongoose')
-
-
-
+const { request } = require('http')
 const app = express()
 
 app.use(express.json())
@@ -20,9 +18,20 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
   const url = process.env.MONGODB_URI
 
 if (!url) {
-  console.error('❌ Missing MONGODB_URI in .env')
+  console.error(' Missing MONGODB_URI in .env')
   process.exit(1)
 }
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
+
 
 app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
@@ -30,45 +39,96 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-// DELETE
-app.delete('/api/notes/:id', (request, response) => {
-  Note.findByIdAndDelete(request.params.id)
-    .then(() => response.status(204).end())
-})
-
 // POST
 app.post('/api/notes', (request, response) => {
   const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ error: 'content missing' })
+  }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
   })
 
-  note.save().then(saved => response.json(saved))
+  note.save()
+  .then(savedNote => {
+    response.json(savedNote)
+  })
+  .catch(error => next(error))
 })
 
-// PUT
-app.put('/api/notes/:id', (request, response) => {
-  const body = request.body
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
 
-  const note = {
-    content: body.content,
-    important: body.important,
+    .catch(error => next(error))
+})
+
+
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    .then(updated => response.json(updated))
+  next(error)
+}
+// error handling middleware should be the last loaded middleware. So we place it after all the routes and other middleware.
+app.use(errorHandler)
+
+// DELETE
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-const unknownEndpoint = (req, res) => {
-  res.status(404).json({ error: 'unknown endpoint' })
-}
+// UPDATE/PUT
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
 
-app.use(unknownEndpoint)
+  Note.findById(request.params.id)
+    .then(note => {
+      if (!note) {
+        return response.status(404).end()
+      }
+
+      note.content = content
+      note.important = important
+
+      return note.save().then((updatedNote) => {
+        response.json(updatedNote)
+      })
+    })
+    .catch(error => next(error))
+})
+
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)  
+
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`)
 })
+
+
+
 
